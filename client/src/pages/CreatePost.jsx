7691b6fed688214d415c6b2fed142b3fbfdc3092
DimaOstanin <xnadab@gmail.com>
2024-02-10 21:43:02 +0200

    before axios

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -6,25 +6,47 @@ import { FormField, Loader } from "../component";
 
 const CreatePost = () => {
   const navigate = useNavigate();
-  const [form, setform] = useState({
+  const [form, setForm] = useState({
     name: "",
     prompt: "",
     photo: "",
   });
   const [generatingImg, setGeneratingImg] = useState(false);
   const [loading, setLoading] = useState(false);
-  const generateImage =(  ) =>{
+  const generateImage = async () => {
+    if(form.prompt){
+      try {
+        setGeneratingImg(true);
+        const response = await fetch('http://localhost:8080/api/v1/dalle',{
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            prompt: form.prompt,
+          }),
+        });
 
+        const data = await response.json();
+        setForm({ ...form, photo: `data:image/jpeg;base64,${data.photo}` });
+      } catch (err) {
+        alert(err);
+      } finally {
+        setGeneratingImg(false);
+      }
+    } else {
+      alert('Please provide proper prompt');
+    }
   }
   const handleSubmit = ()=>{
 
   }
   const handleChange = (e)=>{
-    setform({...form,[e.target.name]: e.target.value})
+    setForm({...form,[e.target.name]: e.target.value})
   }
   const handleSurpriseMe = ()=>{
     const randomPrompt = getRandomPromt(form.prompt);
-    setform({...form,prompt:randomPrompt})
+    setForm({...form,prompt:randomPrompt})
   }
 
   return (
