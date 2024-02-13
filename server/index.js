ffea1c9c3d8d7b3093d8b0c980c98a84fa3d2bb4
DimaOstanin <xnadab@gmail.com>
2024-02-13 21:32:15 +0200

    updating for after render

--- server/index.js
+++ server/index.js
@@ -14,6 +14,9 @@ app.use(express.json({limit: '50mb'}));
 app.use('/api/v1/post',postRoutes);
 app.use('/api/v1/dalle',dalleRoutes);
 
+app.get('/',async (request ,response ) =>{
+    response.send('Hello from DALL-E!')
+})
 
 const startServer = async ()=> {
 
