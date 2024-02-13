d44d40699dc941ac8aff07cb6f45cae6ea722ec5
DimaOstanin <xnadab@gmail.com>
2024-02-13 19:47:09 +0200

    card created

--- client/src/component/Card.jsx
+++ client/src/component/Card.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
 
-const Card = () => {
+import { download } from '../assets';
+import { downloadImage } from '../utils';
+
+
+
+const Card = ({_id,name ,prompt,photo}) => {
   return (
-    <div>Card</div>
+    
+    <div className='rounded-xl group relative shadow-card hover:shadow-cardhover card'>
+      <img className='w-full h-auto object-cover rounded-xl'
+      src={photo}
+      alt={prompt}
+      />
+      <div className='group-hover:flex flex-col max-h-[94.5%] absolute bottom-0 left-3 right-3 bg-[#10131f] m-2 p-4 rounded-md hidden'>
+      <p className='text-white text-md oveflow-y-auto '>{prompt}</p>
+      <div className='mt-5 flex justify-between items-center gap-2'>
+        <div className='flex items-center gap-2 '>
+          <div className='w-full h-7 rounded-full object-cover bg-green-700 flex justify-center items-center text-white text-xs font-bold ' >
+            {'< '+name + ' >'}
+          </div>
+         
+        </div>
+        <button type='button' onClick={() => downloadImage(_id,photo) } className='outline-none bg-transparent border-none'> 
+          <img src={download} alt='download'className='w-7 h-7 object-contain invert' />
+        </button>
+      </div>
+      </div>
+      </div>
   )
 }
 
